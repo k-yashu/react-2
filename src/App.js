551aa83799468e2f47b11c7af4b4434d92186d82
k-yashu <kyashu2003@gmail.com>
2023-10-14 22:55:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Caluclator from "./components/Caluclator.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Caluclator/>
+    </div>
+  );
+}
+
+export default App;
